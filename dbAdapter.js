0900e2434473388917154a857f95522ddf7cb916
aviel <aviel.biton.intern@nielsen.com>
2022-05-15 12:19:48 +0300

    feat(mongoDB): add insert func

--- dbAdapter.js
+++ dbAdapter.js
@@ -1,3 +1,16 @@
-/// text 1 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-//text 2 
+
+const insertDB = function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("makronim");
+    var myobj = { name: "green", price: 40 };
+    dbo.collection("prodoct").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  }
+
+MongoClient.connect(url, insertDB );
